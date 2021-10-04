0935f371dc0815e1201e7e173d49d72f08804b35
nayanbhushankn <nayanbhushankn@dr-ait.org>
2021-10-04 15:53:49 +0530

    Adding mobiles.java

---
+++ Mobiles.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+class Mobiles {
+	HashMap<String,ArrayList<String>> mobiles = new HashMap<>();
+	Mobiles() {}
+	public ArrayList<String> getModels(String company) {
+	
+	}
+}
